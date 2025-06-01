8afb5f69fb723b8047ccd8310d071e9fa70e5b02
Javier Parada <javierparada@pm.me>
2025-06-01 11:05:28 +0200

    Add docs at dates

--- pyvalueobjects/dates/isodate.py
+++ pyvalueobjects/dates/isodate.py
@@ -5,11 +5,23 @@ from pyvalueobjects.strings.string import String
 
 
 class IsoDate(String):
+    """
+    A value object representing a date in ISO format. Inherits from String and validates that the input is a valid ISO date string.
+    """
 
     def __init__(self, value: str):
+        """
+        Initialize the IsoDate object with the given ISO date string.
+        :param value: A string representing a date in ISO format.
+        """
         super().__init__(value)
 
     def _validate(self, value):
+        """
+        Validate that the given value is a valid ISO date string.
+        :param value: The string to validate.
+        :raises: ValueObjectError if the value is not a valid ISO date string.
+        """
         super()._validate(value)
         try:
             datetime.datetime.fromisoformat(value)
