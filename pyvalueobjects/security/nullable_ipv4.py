ec0a386f9710947cab009955fcc56cba1068400a
Javier Parada <javierparada@pm.me>
2025-06-06 10:14:30 +0200

    Add detailed docstrings for NullableIpv4 class and its methods to enhance documentation and clarify functionality.

--- pyvalueobjects/security/nullable_ipv4.py
+++ pyvalueobjects/security/nullable_ipv4.py
@@ -5,6 +5,14 @@ _nullable_cls = build_nullable(Ipv4)
 
 
 class NullableIpv4(_nullable_cls):
+    """
+    A nullable value object representing an IPv4 address.
+    Inherits from a nullable version of Ipv4 and allows the value to be None.
+    """
 
     def __init__(self, value: str = None):
+        """
+        Initialize the NullableIpv4 object with the given IPv4 address string value or None.
+        :param value: An IPv4 address string value, or None.
+        """
         super().__init__(value) 
