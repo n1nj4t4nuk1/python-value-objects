21931c1cfbe7a107a328307dd454963e0ce316b3
Javier Parada <javierparada@pm.me>
2025-06-06 09:58:28 +0200

    Add docs

--- pyvalueobjects/errors/ValueObjectError.py
+++ pyvalueobjects/errors/ValueObjectError.py
@@ -1,4 +1,11 @@
 class ValueObjectError(Exception):
+    """
+    Custom exception raised when a value object validation fails or when an invalid operation is performed on a value object.
+    """
 
     def __init__(self, message: str):
+        """
+        Initialize the ValueObjectError with a specific error message.
+        :param message: A string describing the error that occurred.
+        """
         self.message = message
