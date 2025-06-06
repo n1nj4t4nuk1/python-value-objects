8abbfb9ab8e118e4111965eab8b6065f23780e2e
Javier Parada <javierparada@pm.me>
2025-06-06 10:02:54 +0200

    Add detailed docstrings for NullableNegativeOrZeroInt class and its methods to enhance documentation and clarify functionality.

--- pyvalueobjects/numbers/nullable_negative_or_zero_int.py
+++ pyvalueobjects/numbers/nullable_negative_or_zero_int.py
@@ -5,6 +5,13 @@ _nullable_cls = build_nullable(NegativeOrZeroInt)
 
 
 class NullableNegativeOrZeroInt(_nullable_cls):
+    """
+    A nullable value object representing a negative integer or zero. Inherits from a nullable version of NegativeOrZeroInt and allows the value to be None.
+    """
 
     def __init__(self, value: int = None):
+        """
+        Initialize the NullableNegativeOrZeroInt object with the given negative integer or zero value or None.
+        :param value: A negative integer or zero value, or None.
+        """
         super().__init__(value)
