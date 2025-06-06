fa7c68d5494dbd8ca1ce3e3f35344ac8c90131e3
Javier Parada <javierparada@pm.me>
2025-06-06 10:14:42 +0200

    Add detailed docstrings for NullableMd5Hash class and its methods to enhance documentation and clarify functionality.

--- pyvalueobjects/security/nullable_md5_hash.py
+++ pyvalueobjects/security/nullable_md5_hash.py
@@ -5,6 +5,14 @@ _nullable_cls = build_nullable(Md5Hash)
 
 
 class NullableMd5Hash(_nullable_cls):
+    """
+    A nullable value object representing an MD5 hash.
+    Inherits from a nullable version of Md5Hash and allows the value to be None.
+    """
 
     def __init__(self, value: str = None):
+        """
+        Initialize the NullableMd5Hash object with the given MD5 hash string value or None.
+        :param value: An MD5 hash string value, or None.
+        """
         super().__init__(value) 
