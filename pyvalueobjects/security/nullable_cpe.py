aab203fcc40520987c8bd2d63471965e2c7864a9
Javier Parada <javierparada@pm.me>
2025-06-06 10:14:17 +0200

    Add detailed docstrings for NullableCpe class and its methods to enhance documentation and clarify functionality.

--- pyvalueobjects/security/nullable_cpe.py
+++ pyvalueobjects/security/nullable_cpe.py
@@ -5,6 +5,14 @@ _nullable_cls = build_nullable(Cpe)
 
 
 class NullableCpe(_nullable_cls):
+    """
+    A nullable value object representing a Common Platform Enumeration (CPE) string.
+    Inherits from a nullable version of Cpe and allows the value to be None.
+    """
 
     def __init__(self, value: str = None):
+        """
+        Initialize the NullableCpe object with the given CPE string value or None.
+        :param value: A CPE format string value, or None.
+        """
         super().__init__(value)
