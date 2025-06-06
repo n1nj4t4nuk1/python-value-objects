709668ae29096706dfe12ccd1cb0f83f902bc21e
Javier Parada <javierparada@pm.me>
2025-06-06 10:03:10 +0200

    Add detailed docstrings for NullablePositiveOrZeroInt class and its methods to enhance documentation and clarify functionality.

--- pyvalueobjects/numbers/nullable_positive_or_zero_int.py
+++ pyvalueobjects/numbers/nullable_positive_or_zero_int.py
@@ -5,6 +5,13 @@ _nullable_cls = build_nullable(PositiveOrZeroInt)
 
 
 class NullablePositiveOrZeroInt(_nullable_cls):
+    """
+    A nullable value object representing a positive integer or zero. Inherits from a nullable version of PositiveOrZeroInt and allows the value to be None.
+    """
 
     def __init__(self, value: int = None):
+        """
+        Initialize the NullablePositiveOrZeroInt object with the given positive integer or zero value or None.
+        :param value: A positive integer or zero value, or None.
+        """
         super().__init__(value)
