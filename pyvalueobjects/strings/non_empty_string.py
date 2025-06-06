1478cc7bd4a1d0adf0b2fe9093829751cbc3da6e
Javier Parada <javierparada@pm.me>
2025-06-06 10:08:04 +0200

    Add detailed docstrings for NonEmptyString class and its methods to enhance documentation and clarify functionality.

--- pyvalueobjects/strings/non_empty_string.py
+++ pyvalueobjects/strings/non_empty_string.py
@@ -3,11 +3,23 @@ from pyvalueobjects.strings.string import String
 
 
 class NonEmptyString(String):
+    """
+    A value object representing a non-empty string. Inherits from String and validates that the input is a non-empty string.
+    """
 
     def __init__(self, value: str):
+        """
+        Initialize the NonEmptyString object with the given non-empty string value.
+        :param value: A non-empty string value.
+        """
         super().__init__(value)
 
     def _validate(self, value):
+        """
+        Validate that the given value is a non-empty string.
+        :param value: The value to validate.
+        :raises: ValueObjectError if the value is an empty string.
+        """
         super()._validate(value)
         if value == '':
             raise ValueObjectError('Value must be a non empty strings.')
