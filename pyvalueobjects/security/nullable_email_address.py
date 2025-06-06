7deb63ced89af2840abcdcb69e1202ccc9d6c3c6
Javier Parada <javierparada@pm.me>
2025-06-06 10:14:26 +0200

    Add detailed docstrings for NullableEmailAddress class and its methods to enhance documentation and clarify functionality.

--- pyvalueobjects/security/nullable_email_address.py
+++ pyvalueobjects/security/nullable_email_address.py
@@ -5,6 +5,14 @@ _nullable_cls = build_nullable(EmailAddress)
 
 
 class NullableEmailAddress(_nullable_cls):
+    """
+    A nullable value object representing an email address.
+    Inherits from a nullable version of EmailAddress and allows the value to be None.
+    """
 
     def __init__(self, value: str = None):
+        """
+        Initialize the NullableEmailAddress object with the given email address string value or None.
+        :param value: An email address string value, or None.
+        """
         super().__init__(value) 
