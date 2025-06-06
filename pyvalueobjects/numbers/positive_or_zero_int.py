c2f1889b3e6fce3f4792047112055c02a5d04e5e
Javier Parada <javierparada@pm.me>
2025-06-06 10:03:24 +0200

    Add detailed docstrings for PositiveOrZeroInt class and its methods to enhance documentation and clarify functionality.

--- pyvalueobjects/numbers/positive_or_zero_int.py
+++ pyvalueobjects/numbers/positive_or_zero_int.py
@@ -3,11 +3,23 @@ from pyvalueobjects.numbers.int import Int
 
 
 class PositiveOrZeroInt(Int):
+    """
+    A value object representing a positive integer or zero. Inherits from Int and validates that the input is a positive integer or zero.
+    """
 
     def __init__(self, value: int):
+        """
+        Initialize the PositiveOrZeroInt object with the given positive integer or zero value.
+        :param value: A positive integer or zero value.
+        """
         super().__init__(value)
 
     def _validate(self, value):
+        """
+        Validate that the given value is a positive integer or zero.
+        :param value: The value to validate.
+        :raises: ValueObjectError if the value is not a positive integer or zero.
+        """
         super()._validate(value)
         if value < 0:
             raise ValueObjectError('Value must be greater or equals to 0.')
