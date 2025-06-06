6303a5dc33396263f43cb5d898525639d1bf8bfd
Javier Parada <javierparada@pm.me>
2025-06-06 10:02:49 +0200

    Add detailed docstrings for NullableNegativeInt class and its methods to enhance documentation and clarify functionality.

--- pyvalueobjects/numbers/nullable_negative_int.py
+++ pyvalueobjects/numbers/nullable_negative_int.py
@@ -5,6 +5,13 @@ _nullable_cls = build_nullable(NegativeInt)
 
 
 class NullableNegativeInt(_nullable_cls):
+    """
+    A nullable value object representing a negative integer. Inherits from a nullable version of NegativeInt and allows the value to be None.
+    """
 
     def __init__(self, value: int = None):
+        """
+        Initialize the NullableNegativeInt object with the given negative integer value or None.
+        :param value: A negative integer value, or None.
+        """
         super().__init__(value)
