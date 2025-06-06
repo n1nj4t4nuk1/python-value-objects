9a398cc46cfe05916a3449480469318f1d281117
Javier Parada <javierparada@pm.me>
2025-06-06 10:14:34 +0200

    Add detailed docstrings for NullableIpv6 class and its methods to enhance documentation and clarify functionality.

--- pyvalueobjects/security/nullable_ipv6.py
+++ pyvalueobjects/security/nullable_ipv6.py
@@ -5,6 +5,14 @@ _nullable_cls = build_nullable(Ipv6)
 
 
 class NullableIpv6(_nullable_cls):
+    """
+    A nullable value object representing an IPv6 address.
+    Inherits from a nullable version of Ipv6 and allows the value to be None.
+    """
 
     def __init__(self, value: str = None):
+        """
+        Initialize the NullableIpv6 object with the given IPv6 address string value or None.
+        :param value: An IPv6 address string value, or None.
+        """
         super().__init__(value) 
