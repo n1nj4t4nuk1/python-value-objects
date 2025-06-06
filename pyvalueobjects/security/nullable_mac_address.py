6a43f174775bac8734e2afe78a0ed109b4e79a3d
Javier Parada <javierparada@pm.me>
2025-06-06 10:14:38 +0200

    Add detailed docstrings for NullableMacAddress class and its methods to enhance documentation and clarify functionality.

--- pyvalueobjects/security/nullable_mac_address.py
+++ pyvalueobjects/security/nullable_mac_address.py
@@ -5,6 +5,14 @@ _nullable_cls = build_nullable(MacAddress)
 
 
 class NullableMacAddress(_nullable_cls):
+    """
+    A nullable value object representing a MAC address.
+    Inherits from a nullable version of MacAddress and allows the value to be None.
+    """
 
     def __init__(self, value: str = None):
+        """
+        Initialize the NullableMacAddress object with the given MAC address string value or None.
+        :param value: A MAC address string value, or None.
+        """
         super().__init__(value) 
