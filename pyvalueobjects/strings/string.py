1a724696c8aa371791bb992028fdd88d40ea17a9
Javier Parada <javierparada@pm.me>
2025-06-06 10:08:30 +0200

    Add detailed docstrings for String class and its methods to enhance documentation and clarify functionality.

--- pyvalueobjects/strings/string.py
+++ pyvalueobjects/strings/string.py
@@ -3,13 +3,25 @@ from pyvalueobjects.errors.ValueObjectError import ValueObjectError
 
 
 class String(ValueObject):
+    """
+    A value object representing a string. Inherits from ValueObject and validates that the input is a string.
+    """
 
     _ALLOWED_INPUT_TYPES = {str, int, float, bool}
 
     def __init__(self, value: str):
+        """
+        Initialize the String object with the given string value.
+        :param value: A string value.
+        """
         super().__init__(value)
 
     def _validate(self, value):
+        """
+        Validate that the given value is a string.
+        :param value: The value to validate.
+        :raises: ValueObjectError if the value is not a string.
+        """
         super()._validate(value)
         input_type = type(value)
         if input_type != str:
