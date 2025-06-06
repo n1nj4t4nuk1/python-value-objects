7b79fe1abff03f8b39b0bd73b1f7fa904448a101
Javier Parada <javierparada@pm.me>
2025-06-06 10:02:44 +0200

    Add detailed docstrings for NullableInt class and its methods to enhance documentation and clarify functionality.

--- pyvalueobjects/numbers/nullable_int.py
+++ pyvalueobjects/numbers/nullable_int.py
@@ -5,6 +5,13 @@ _nullable_cls = build_nullable(Int)
 
 
 class NullableInt(_nullable_cls):
+    """
+    A nullable value object representing an integer. Inherits from a nullable version of Int and allows the value to be None.
+    """
 
     def __init__(self, value: int = None):
+        """
+        Initialize the NullableInt object with the given integer value or None.
+        :param value: An integer value, or None.
+        """
         super().__init__(value)
